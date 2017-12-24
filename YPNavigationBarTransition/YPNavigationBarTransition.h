776e4484429f9b6ab7b53debb6c1231685a95120
yiplee <guoyinl@gmail.com>
2017-12-24 11:00:14 +0800

    Initial Commit

---
+++ YPNavigationBarTransition/YPNavigationBarTransition.h
@@ -0,0 +1,19 @@
+//
+//  YPNavigationBarTransition.h
+//  YPNavigationBarTransition
+//
+//  Created by Guoyin Lee on 24/12/2017.
+//  Copyright © 2017 yiplee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YPNavigationBarTransition.
+FOUNDATION_EXPORT double YPNavigationBarTransitionVersionNumber;
+
+//! Project version string for YPNavigationBarTransition.
+FOUNDATION_EXPORT const unsigned char YPNavigationBarTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YPNavigationBarTransition/PublicHeader.h>
+
+
