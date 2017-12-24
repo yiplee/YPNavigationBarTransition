70c23caf5a18c7f9a0383f8d751d02aac9b2b05d
yiplee <guoyinl@gmail.com>
2017-12-24 11:03:29 +0800

    init project

---
+++ YPNavigationBarTransition-Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YPNavigationBarTransition-Example
+//
+//  Created by Guoyin Lee on 24/12/2017.
+//  Copyright © 2017 yiplee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YPNavigationBarTransition-Example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YPNavigationBarTransition-Example
+//
+//  Created by Guoyin Lee on 24/12/2017.
+//  Copyright © 2017 yiplee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
