1afcac46a16a8d5f5a6e9f92cb1cdf9cb8582c6a
yiplee <guoyinl@gmail.com>
2017-12-25 21:39:10 +0800

    best practice of NavigationController

---
+++ YPNavigationBarTransition-Example/YPNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  YPNavigationController.h
+//  YPNavigationBarTransition-Example
+//
+//  Created by Guoyin Lee on 25/12/2017.
+//  Copyright © 2017 yiplee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YPNavigationController : UINavigationController
+
+@end
---
+++ YPNavigationBarTransition-Example/YPNavigationController.m
@@ -0,0 +1,67 @@
+//
+//  YPNavigationController.m
+//  YPNavigationBarTransition-Example
+//
+//  Created by Guoyin Lee on 25/12/2017.
+//  Copyright © 2017 yiplee. All rights reserved.
+//
+
+#import "YPNavigationController.h"
+@import YPNavigationBarTransition;
+
+@interface YPNavigationController ()<YPNavigationBarConfigureStyle>
+
+@property (nonatomic, strong) YPNavigationBarTransitionCenter *transitionCenter;
+
+@end
+
+@implementation YPNavigationController
+
+- (instancetype) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        [self createTransitionCenter];
+    }
+    
+    return self;
+}
+
+- (instancetype) initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self createTransitionCenter];
+    }
+    
+    return self;
+}
+
+- (void) createTransitionCenter {
+    NSParameterAssert(!_transitionCenter && !self.delegate);
+    
+    _transitionCenter = [[YPNavigationBarTransitionCenter alloc] initWithDefaultBarConfiguration:self];
+    _transitionCenter.navigationController = self;
+}
+
+- (void) setDelegate:(id<UINavigationControllerDelegate>)delegate {
+    _transitionCenter.navigationDelegate = delegate;
+}
+
+- (id<UINavigationControllerDelegate>) delegate {
+    return _transitionCenter.navigationDelegate;
+}
+
+#pragma mark - YPNavigationBarConfigureStyle
+
+- (YPNavigationBarConfigurations) yp_navigtionBarConfiguration {
+    return YPNavigationBarConfigurationsDefault | YPNavigationBarStyleBlack | YPNavigationBarBackgroundStyleColor;
+}
+
+- (UIColor *) yp_navigationBarTintColor {
+    return [UIColor whiteColor];
+}
+
+- (UIColor *) yp_navigationBackgroundColor {
+    return [UIColor blackColor];
+}
+
+@end
