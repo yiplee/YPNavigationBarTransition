e1c1076b7485c13e845a91cfeda0687379996a5b
yiplee <guoyinl@gmail.com>
2017-12-25 21:47:46 +0800

    build demo

--- YPNavigationBarTransition-Example/YPDemoViewController.h
+++ YPNavigationBarTransition-Example/YPDemoViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  YPDemoViewController.h
 //  YPNavigationBarTransition-Example
 //
-//  Created by Guoyin Lee on 24/12/2017.
+//  Created by Guoyin Lee on 25/12/2017.
 //  Copyright © 2017 yiplee. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface YPDemoViewController : UIViewController
 
 @end
-
---
+++ YPNavigationBarTransition-Example/YPDemoViewController.m
@@ -0,0 +1,37 @@
+//
+//  YPDemoViewController.m
+//  YPNavigationBarTransition-Example
+//
+//  Created by Guoyin Lee on 25/12/2017.
+//  Copyright © 2017 yiplee. All rights reserved.
+//
+
+#import "YPDemoViewController.h"
+
+@interface YPDemoViewController ()
+
+@end
+
+@implementation YPDemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
