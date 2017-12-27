55fc9997526f9fd1cc8da2c3a1a7fcf1e35f63f3
yiplee <guoyinl@gmail.com>
2017-12-27 20:11:58 +0800

    rename scheme

--- run_unit_test.sh
+++ run_unit_test.sh
@@ -1,3 +1,3 @@
 bundler install
 bundle exec pod install
-xcodebuild analyze test -workspace YPNavigationBarTransition.xcworkspace -scheme YPNavigationBarTransition -sdk iphonesimulator -destination "OS=11.2,name=iPhone 8" -configuration Debug ONLY_ACTIVE_ARCH=NO CODE_SIGNING_REQUIRED=NO | bundle exec xcpretty -c
+xcodebuild analyze test -workspace YPNavigationBarTransition.xcworkspace -scheme YPNavigationBarTransitionLibrary -sdk iphonesimulator -destination "OS=11.2,name=iPhone 8" -configuration Debug ONLY_ACTIVE_ARCH=NO CODE_SIGNING_REQUIRED=NO | bundle exec xcpretty -c
